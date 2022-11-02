7ac04a081acf65cec414d2649f0186d0e152aa14
Brayan <alfredochabes1@hotmail.com>
2022-11-02 12:02:46 -0400

    added schemas paths to server.js plus called apollo

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const { apolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const server = new apolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
